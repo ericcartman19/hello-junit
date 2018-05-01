6e8e64a678917545b0fdb4b512452cb3eed4d06b
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-05-01 23:56:35 +0200

    use @Before and @After

--- src/main/java/com/valoyes/junitintro/GreetingImpl.java
+++ src/main/java/com/valoyes/junitintro/GreetingImpl.java
@@ -1,9 +1,15 @@
 package com.valoyes.junitintro;
 
+import org.springframework.util.StringUtils;
+
 public class GreetingImpl implements Greeting {
 
 	@Override
 	public String greet(String name) {
+		
+		if(StringUtils.isEmpty(name)) {
+			throw new IllegalArgumentException();
+		}
 		return "Hello " + name;
 	}
 	
--- src/test/java/com/valoyes/junitintro/GreetingImplTest.java
+++ src/test/java/com/valoyes/junitintro/GreetingImplTest.java
@@ -1,16 +1,26 @@
 package com.valoyes.junitintro;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GreetingImplTest {
 
 	private static final String GREET_JUNIT = "Hello Junit";
+	private Greeting greeting;
 
+	@Before
+	public void setup() {
+		System.out.println("Setup");
+		greeting = new GreetingImpl();
+	}
+	
 	@Test
 	public void casPassant_shouldReturnAValidOutput() {
-
+		System.out.println("casPassant_shouldReturnAValidOutput");
 		Greeting greeting = new GreetingImpl();
 		String resultado = greeting.greet("Junit");
 		
@@ -18,6 +28,26 @@ public class GreetingImplTest {
 		assertNotNull(resultado);
 		assertEquals(GREET_JUNIT, resultado);
 		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void casNonPassant_NameNullThrowsException() {
+		System.out.println("casNonPassant_NameNullThrowsException");
+		greeting = new GreetingImpl();
+		greeting.greet(null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void casNonPassant_NameBlankThrowsException() {
+		System.out.println("casNonPassant_NameBlankThrowsException");
+		greeting = new GreetingImpl();
+		greeting.greet("");
+	}
+	
+	@After 
+	public void tearDown() {
+		System.out.println("Teardown");
+		greeting = null;
 	}
 
 }
